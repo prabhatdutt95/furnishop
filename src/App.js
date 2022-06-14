057a8b30cf28b553048e6ed4b9aaa9ca539a4b01
Prabhat Dutt <prabhatdutt95@gmail.com>
2022-06-15 02:35:24 +0530

    Added Contact. Started with Footer section.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/general.css";
 import "./styles/navbar.css";
 import { AiFillCheckCircle } from "react-icons/ai";
+import { FaArrowRight } from "react-icons/fa";
 
 import React, { useEffect, useState } from "react";
 
@@ -146,15 +147,9 @@ function App() {
       </section>
       {/* About section ends */}
 
-      {/* About section start */}
+      {/* About Features start */}
       <section id="features" className="wrapper">
-        <div className="screen">
-          <img
-            src={require("./assets/images/about.jpg")}
-            alt="Grey Sofa with a brown coffee table"
-          />
-        </div>
-        <div className="screen" style={{ maxWidth: "50%", padding: "25px" }}>
+        <div className="screen" style={{ maxWidth: "30%", padding: "25px" }}>
           <h2
             style={{
               fontSize: "35px",
@@ -162,89 +157,106 @@ function App() {
             }}
             className="mb-20"
           >
-            We Create your home more aestetic
+            New In Store Now
           </h2>
-          <p className="mb-20">
-            Furnitre power is a software as services for multiperpose business
-            management system,
-          </p>
           <div className="mb-20">
-            <h4>
-              <strong>Valuation Services</strong>
-            </h4>
             <span style={{ display: "flex", alignItems: "center" }}>
-              <AiFillCheckCircle
-                style={{ height: "15px", marginRight: "5px" }}
-              />
-              Sometimes features require a short description. This can be
-              detailed description
+              Get the latest items immediately with promo prices
             </span>
           </div>
-          <div>
-            <h4>
-              <strong>Development of Furniture Models</strong>
-            </h4>
-            <span style={{ display: "flex", alignItems: "center" }}>
-              <AiFillCheckCircle
-                style={{ height: "15px", marginRight: "5px" }}
-              />
-              Sometimes features require a short description. This can be
-              detailed description
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Check All
+              <FaArrowRight style={{ height: "15px", marginLeft: "5px" }} />
             </span>
           </div>
+        </div>
+        <div className="screen">
+          <figure>
+            <img
+              src={require("./assets/images/bed.jpg")}
+              alt="Grey Sofa with a brown coffee table"
+            />
+            <figcaption>Chair</figcaption>
+          </figure>
+        </div>
+        <div className="screen">
+          <figure>
+            <img
+              src={require("./assets/images/lighting.jpg")}
+              alt="Grey Sofa with a brown coffee table"
+            />
+            <figcaption>Lighting</figcaption>
+          </figure>
+        </div>
+        <div className="screen">
+          <figure>
+            <img
+              src={require("./assets/images/cupboard.jpg")}
+              alt="Grey Sofa with a brown coffee table"
+            />
+            <figcaption>Cupboard</figcaption>
+          </figure>
         </div>
       </section>
       {/* About section ends */}
 
-      {/* About section start */}
-      <section id="contact" className="wrapper">
-        <div className="screen">
-          <img
-            src={require("./assets/images/about.jpg")}
-            alt="Grey Sofa with a brown coffee table"
-          />
-        </div>
-        <div className="screen" style={{ maxWidth: "50%", padding: "25px" }}>
-          <h2
-            style={{
-              fontSize: "35px",
-              lineHeight: "40px",
-            }}
-            className="mb-20"
-          >
-            We Create your home more aestetic
-          </h2>
-          <p className="mb-20">
-            Furnitre power is a software as services for multiperpose business
-            management system,
-          </p>
-          <div className="mb-20">
-            <h4>
-              <strong>Valuation Services</strong>
-            </h4>
-            <span style={{ display: "flex", alignItems: "center" }}>
-              <AiFillCheckCircle
-                style={{ height: "15px", marginRight: "5px" }}
-              />
-              Sometimes features require a short description. This can be
-              detailed description
-            </span>
+      {/* About Contact start */}
+      <footer id="contact" className="footer-bg-img wrapper">
+        <div
+          className="screen"
+          style={{
+            left: "25vw",
+            bottom: "10vh",
+            maxWidth: "40%",
+            color: "white",
+            textAlign: "start",
+          }}
+        >
+          <h1 className="text-size-xl mb-20">
+            Get more discount Off your order
+          </h1>
+          <p>Join our mailing list</p>
+          <div style={{ display: "flex", marginTop: "5px" }}>
+            <input
+              type="number"
+              placeholder="Your email address"
+              style={{
+                width: "35vw",
+                height: "50px",
+                borderRadius: "8px",
+                border: "none",
+                textAlign: "center",
+              }}
+            />
+            <button
+              style={{
+                marginLeft: "5px",
+                width: "12vw",
+                background: "#23262F",
+                color: "white",
+                border: "none",
+                borderRadius: "8px",
+              }}
+            >
+              Shop Now
+            </button>
           </div>
+        </div>
+        <section className="wrapper">
           <div>
-            <h4>
-              <strong>Development of Furniture Models</strong>
-            </h4>
-            <span style={{ display: "flex", alignItems: "center" }}>
-              <AiFillCheckCircle
-                style={{ height: "15px", marginRight: "5px" }}
-              />
-              Sometimes features require a short description. This can be
-              detailed description
-            </span>
+            <h3>FurniShop</h3>
           </div>
-        </div>
-      </section>
-      {/* About section ends */}
+        </section>
+      </footer>
+      {/* About Contact ends */}
     </div>
   );
 }
